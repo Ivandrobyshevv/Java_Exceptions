51c9df2ad382019cc8fb169656559486bd86a18d
Ivan Drobyshev <drobyshev_ivan00@mail.ru>
2022-12-30 08:11:30 +0500

    added hm_2

---
+++ src/main/java/HM_2/task_2/Main.java
@@ -0,0 +1,27 @@
+package HM_2.task_2;
+
+/**
+ * Если необходимо, исправьте данный код <br>
+ * (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+ * <p>
+ * Внесем исправления таким образом, чтобы код отрвбатывал без возникновения исключений.
+ * <p>
+ * Возможные варианты:
+ * - полностью исправить код так, чтобы не происходило иск
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+        try {
+            int d = 1;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
+    }
+}
